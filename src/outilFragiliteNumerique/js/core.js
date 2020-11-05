dbc0def72f46fb556d78df8c86a73f0509f79f83
Louvrier Arthur <arthur.louvrier@etudiant.univ-lr.fr>
2020-11-05 21:54:25 +0100

    Réparation de l'erreur de florian

--- src/outilFragiliteNumerique/js/core.js
+++ src/outilFragiliteNumerique/js/core.js
@@ -1,4 +1,3 @@
-
 cache= {};
 
 $(function(){
@@ -13,47 +12,55 @@ $(function(){
 		}else{
 			// issue an AJAX request
 			$.getJSON("apiAccess.php", { cityName: searchTerm},
-				function(datas){ // callack function
-										
-						setHtml(datas);
-						cache[searchTerm]=datas;
-				
+				function(data){ // callack function
+					if(data!=null){					
+						setHtml(data);
+						cache[searchTerm]=data;
+                    }
+                    
+                    if(data === null || data["Type"]==="EMPTY")
+                    {
+                        alert("recherche incorrecte !");
+                    }
+
+
+
+                    console.log("Object contents:");
+                    for (const [key, value] of Object.entries(data)) {
+                        console.log(`${key}: ${value}`);
+                    }
 				}	
 			);
 		}
         
         return false; // to prevent the default action
     });
-
-    var arraySugg = ["17000","17001","17002","17003","17aaaa","172000", "bonjour", "bonmatin","bonsoir"];
-    $(".searchTerm").autocomplete({minLength:1, source: "autoComplete.php"}); //"autoComplete.php",
+    
+    $(".searchTerm").autocomplete({minLength:2, source: "autoComplete.php"});
 });
+
 
 function setHtml(data){
 	//Edit the html with the result
-				console.log(data);
-				$("#nameCommune").html(data.Nom);
-                $("#comGlobal").html(data.ScoreGlobal);
-                $("#comAccesInfo").html(data.ScoreAccesInfo);
-                $("#comAccesNum").html(data.ScoreAccesNum);
-                $("#comUsageNum").html(data.ScoreUsageNum);
-                $("#comCompAdmin").html(data.ScoreCompAdmin);
-				$("#iris").html(data.IdIris);
+	console.log(data);
+	$("#nameCommune").html(data.Nom);
+    $("#comGlobal").html(data.ScoreGlobal);
+    $("#comAccesInfo").html(data.ScoreAccesInfo);
+    $("#comAccesNum").html(data.ScoreAccesNum);
+    $("#comUsageNum").html(data.ScoreUsageNum);
+    $("#comCompAdmin").html(data.ScoreCompAdmin);
 
-                $("#nameRegion").html(data.NomRegion);
-                $("#regGlobal").html(data.ScoreRegion);
-                $("#regAccesInfo").html(data.ScoreRegAccesInfo);
-                $("#regAccesNum").html(data.ScoreRegAccesNum);
-                $("#regUsageNum").html(data.ScoreRegUsageNum);
-                $("#regCompAdmin").html(data.ScoreRegCompAdmin);
+    $("#nameRegion").html(data.NomRegion);
+    $("#regGlobal").html(data.ScoreRegion);
+    $("#regAccesInfo").html(data.ScoreRegAccesInfo);
+    $("#regAccesNum").html(data.ScoreRegAccesNum);
+    $("#regUsageNum").html(data.ScoreRegUsageNum);
+    $("#regCompAdmin").html(data.ScoreRegCompAdmin);
 
-                $("#nameDept").html(data.NomDept);
-                $("#deptGlobal").html(data.ScoreDept);
-                $("#deptAccesInfo").html(data.ScoreDeptAccesInfo);
-                $("#deptAccesNum").html(data.ScoreDeptAccesNum);
-                $("#deptUsageNum").html(data.ScoreDeptUsageNum);
-                $("#deptCompAdmin").html(data.ScoreDeptCompAdmin);
-				
-	
-	
+    $("#nameDept").html(data.NomDept);
+    $("#deptGlobal").html(data.ScoreDept);
+    $("#deptAccesInfo").html(data.ScoreDeptAccesInfo);
+    $("#deptAccesNum").html(data.ScoreDeptAccesNum);
+    $("#deptUsageNum").html(data.ScoreDeptUsageNum);
+    $("#deptCompAdmin").html(data.ScoreDeptCompAdmin);
 }
